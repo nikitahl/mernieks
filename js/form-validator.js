4139d9d38054c7c703eca3d4b5ca707181395a3a
nikitahl <n.hlopov@gmail.com>
2016-04-10 20:03:14 +0300

    Refactored form validation

--- js/form-validator.js
+++ js/form-validator.js
@@ -1,68 +1,100 @@
 var formValidation = {
-
-  submitForm: function() {
-
-    var formFields = this.elements,  // select form fields
-        // get fields values
-        fieldValues = {
-          userName: formFields['name'],
-          userEmail: formFields['email'],
-          userMessage: formFields['textarea']
-        },
-        emailCheck = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,  //valid email symbols
-        messageField = document.getElementById('message-field'),
-        errorMessage = [
-          'Name field is empty',
-          'Email field is empty',
-          'Text field is empty'
-        ],
-        filledValues = [];
-
+  contactForm: document.getElementById("contact-form"),
+  messageField: document.getElementById('message-field'),
+  emailCheck: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+  errorMessage: {
+    name: 'Name field is empty',
+    email: 'Email field is empty',
+    message: 'Text field is empty'
+  },
+  isValidForm: false,
+  fieldsValues: {},
+  refreshValues: function() {
+    for (var prop in this.fieldsValues){
+        if (this.fieldsValues.hasOwnProperty(prop)){
+            delete this.fieldsValues[prop];
+        }
+    }
+  },
+  clearForm: function (input) {
+    var messages = this.messageField;
     //clear message field from previous messages
-    while (messageField.firstChild) {
-      messageField.removeChild(messageField.firstChild);
+    while (messages.firstChild) {
+      messages.removeChild(messages.firstChild);
     }
     // clear error borders
-    for ( var key in fieldValues ) {
-      fieldValues[key].classList.remove("is-invalid-data");
+    for ( var i = 0; i < input.length; i++ ) {
+      input[i].classList.remove("is-invalid-data");
     }
-    // evaluate values
-    for ( var val in fieldValues ) {
-      if ( fieldValues[val].value === "" ) {
-        var i = Object.keys(fieldValues).indexOf(val); // get the loop counter/index
-        errorMessageText = document.createElement('p');
-        errorMessageText.innerHTML = errorMessage[i]; //write appropriate array value to paragraph
-        messageField.appendChild(errorMessageText);
-        fieldValues[val].classList.add("is-invalid-data");
-        // console.log(errorMessage[i]);
-      } else {
-        filledValues.push(fieldValues[val].value);
+  },
+  validateEmail: function(el, val) {
+    var errorMessageText = document.createElement('p');
+    if ( !this.emailCheck.test(val) ) {
+      errorMessageText.textContent = "Please enter valid email";
+      this.messageField.appendChild(errorMessageText);
+      el.classList.add("is-invalid-data");
+    } else {
+      this.isValidForm = true;
+    }
+  },
+  displayError: function(el) {
+    var errorMessageText = document.createElement('p');
+    for (var val in this.errorMessage) {
+      if ( el.getAttribute("name") === val ) {
+        errorMessageText.textContent = this.errorMessage[val];
       }
     }
-
-
-    if ( filledValues.length == Object.keys(fieldValues).length ) {
-
-      if ( !emailCheck.test(filledValues[1]) ) {
-        errorMessageText = document.createElement('p');
-        errorMessageText.innerHTML = 'Please enter a valid email';
-        messageField.appendChild(errorMessageText);
-        // fieldValues[1].classList.add("is-invalid-data");
-        // console.log( "Enter a valid email" );
+    this.messageField.appendChild(errorMessageText);
+    el.classList.add("is-invalid-data");
+  },
+  returnValues: function() {
+    if (this.isValidForm) {
+      var successMessageText = document.createElement('p');
+      successMessageText.textContent = "Thank You!";
+      this.messageField.appendChild(successMessageText);
+      return this.fieldsValues;
+    }
+  },
+  evaluateFields: function(el){
+    var emailValue,
+        emailField;
+    for (var i = 0; i < el.length; i++) {
+      var fieldName = el[i].getAttribute("name");
+      if ( !el[i].value.trim() ) {
+        this.displayError(el[i]);
       } else {
-        errorMessageText = document.createElement('p');
-        errorMessageText.innerHTML = 'Thank You!';
-        messageField.appendChild(errorMessageText);
-        // console.log( "Thank you!" );
+        this.fieldsValues[fieldName] = el[i].value.trim();
+        if ( fieldName === "email") {
+          emailValue = el[i].value.trim();
+          emailField = el[i];
+        }
       }
     }
-
-    return false;
+    if (Object.keys(this.fieldsValues).length === 3) {
+      this.validateEmail(emailField, emailValue);
+    }
+  },
+  getFormFields: function(e){
+    e.preventDefault();
+    var formFields = this.contactForm.elements,
+        formInputs = [];
+    this.clearForm(formFields);
+    this.refreshValues();
+    for (var i = 0; i < formFields.length; i++) {
+      if ( formFields[i].hasAttribute("name") && formFields[i].getAttribute("name") != "submit" ) {
+        formInputs.push(formFields[i]);
+      }
+    }
+    this.evaluateFields(formInputs);
+    this.returnValues();
+  },
+  submitForm: function() {
+    var _this = this;
+    this.contactForm.addEventListener("submit", _this.getFormFields.bind(_this));
   },
   init: function() {
-    document.getElementById('contact-form').onsubmit = this.submitForm;
+    this.submitForm();
   }
-
 }
 
 formValidation.init();
